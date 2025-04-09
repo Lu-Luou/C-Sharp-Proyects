5d52c411cea4aa80c57b9f70ed85b551ec6731b3
Lu-Luou <rubertohl07@gmail.com>
2025-04-08 21:16:57 -0300

    Cronometro done

---
+++ Cronometro/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Cronometro
+{
+    private int _segundos;
+    private int _minutos;
+
+    public void Reiniciar()
+    {
+        _segundos = 0;
+        _minutos = 0;
+    }
+
+    public void IncrementarTiempo()
+    {
+        _segundos++;
+        if (_segundos >= 60)
+        {
+            _minutos++;
+            _segundos -= 60;
+        }
+    }
+
+    public string MostrarTiempo()
+    {
+        return $"{_minutos} minutos, {_segundos} segundos";
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Cronometro cronometro = new Cronometro();
+        for (int i = 0; i < 5000; i++)
+        {
+            cronometro.IncrementarTiempo();
+        }
+        Console.WriteLine(cronometro.MostrarTiempo());
+    }
+}
